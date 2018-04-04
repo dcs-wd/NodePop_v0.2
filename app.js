9d3a1c66a0668217be453371bda01bf980d040f1
Daniel Castillo <dcsepulveda@hotmail.es>
2018-04-05 01:18:24 +0200

    configuración de node para la app

--- app.js
+++ app.js
@@ -6,34 +6,91 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-/* jshint ignore:start */
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const jwtAuth = require('./lib/jwtAuth');
+
 const db = require('./lib/connectMongoose');
-/* jshint ignore:end */
 
 // Cargamos las definiciones de todos nuestros modelos
 require('./models/Anuncio');
+const Usuario = require('./models/Usuario');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Cambio motor a html
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
+
+// Global Template variables
+app.locals.title = 'NodePop';
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// middleware de estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Global Template variables
-app.locals.title = 'NodePop';
+// i18n config
+const i18n = require('./lib/i18nConfigure')();
+app.use(i18n.init);
+
+// login controller
+const loginController = require('./routes/loginController');
 
 // Web
 app.use('/', require('./routes/index'));
 app.use('/anuncios', require('./routes/anuncios'));
 
 // API v1
-app.use('/apiv1/anuncios', require('./routes/apiv1/anuncios'));
+app.use('/apiv1/anuncios', jwtAuth(), require('./routes/apiv1/anuncios'));
+app.use('/loginJWT', loginController.postLoginJWT);
+
+// middleware de control de sesiones
+app.use(session({
+  name: 'token-session',
+  secret: 'askjdahjdhakdhaskdas7dasd87asd89as7d89asd7a9s8dhjash',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 2 * 24 * 60 * 60 * 1000, httpOnly: true }, // dos dias de inactividad
+  store: new MongoStore({
+    // como conectarse a mi base de datos
+    url: 'mongodb://localhost/nodepop' // fix issue https://github.com/jdesboeufs/connect-mongo/issues/277
+    // mongooseConnection: conn
+  })
+}));
+
+app.use(async (req, res, next) => {
+  try {
+    // si el usuario está logado, cargamos en req.user el objeto de usuario desde la base de datos
+    // para que los siguientes middlewares lo puedan usar
+    req.user = req.session.authUser ? await Usuario.findById(req.session.authUser._id) : null;
+    next();
+  } catch (err) {
+    next(err);
+    return;
+  }
+});
+
+// Config app web
+app.use(function (req, res, next) {
+  next();
+});
+
+app.get('/login', loginController.index);
+app.post('/login', loginController.post);
+app.get('/logout', loginController.logout);
+
+app.use('/', require('./routes/index'));
+app.use('/about', require('./routes/about'));
+app.use('/lang', require('./routes/lang'));
+
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -43,13 +100,13 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  
+app.use(function (err, req, res, next) {
+
   if (err.array) { // validation error
     err.status = 422;
     const errInfo = err.array({ onlyFirstError: true })[0];
     err.message = isAPI(req) ?
-      { message: 'not valid', errors: err.mapped()}
+      { message: 'not valid', errors: err.mapped() }
       : `not valid - ${errInfo.param} ${errInfo.msg}`;
   }
 
@@ -59,14 +116,14 @@ app.use(function(err, req, res, next) {
 
   // si es un 500 lo pinto en el log
   if (err.status && err.status >= 500) console.error(err);
-  
-  // si es una petición al API respondo JSON...
+
+  // si es una petición de API, respondemos con JSON
   if (isAPI(req)) {
     res.json({ success: false, error: err.message });
     return;
   }
 
-  // ...y si no respondo con HTML...
+  // Respondo con una página de error
 
   // set locals, only providing error in development
   res.locals.message = err.message;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,90 @@
-'use strict';
+var express = require('express');
+var router = express.Router();
+const i18n = require('../lib/i18nConfigure')();
+const sessionAuth = require('../lib/sessionAuth');
+const Usuario = require('../models/Usuario');
 
-const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+// cargamos librería de validaciones
+const { query, validationResult } = require('express-validator/check');
+
+// cargamos objeto de upload
+const upload = require('../lib/uploadConfig');
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/', sessionAuth(), function(req, res, next) {
+  //res.render('index', { title: 'Nodeapi' });
+
+  const segundo = (new Date()).getSeconds();
+
+  res.locals.valor = 123;
+  res.locals.texto = `<script>alert("${res.__('hola, he conseguido inyectar codigo')}")</script>`;
+  res.locals.condicion = {
+    segundo: segundo,
+    estado: segundo % 2 === 0
+  };
+  res.locals.users = [
+    { name: 'Smith', age: 20 },
+    { name: 'Thomas', age: 34 },
+    { name: 'Jones', age: 47 }
+  ];
+  res.render('index');
+});
+
+router.post('/sendemail', async (req, res, next) => {
   try {
-    const filename = path.join(__dirname, '../README.md');
-    const readme = await new Promise((res, rej) => 
-      fs.readFile(filename, 'utf8', (err, data) => err ? rej(err) : res(data) )
-    );
-    res.render('index', { readme });
-  } catch (err) { return next(err); }
+    
+    await req.user.sendMail('Nodeapi', 'Asunto de prueba', 'Correo de prueba');
+    
+    res.redirect('/');
+  } catch(err) {
+    next(err);
+    return;
+  }
+});
+
+router.post('/upload', upload.single('imagen'), (req, res, next) => {
+  console.log('upload:', req.file);
+  res.redirect('/');
+});
+
+router.get('/paramenruta/:id', sessionAuth(), (req, res, next) => {
+  console.log('req.params', req.params);
+  const id = req.params.id;
+  res.send('ok');
+});
+
+router.get('/paramopcional/:dato?', (req, res, next) => {
+  console.log('req.params', req.params);
+  res.send('ok');
+});
+
+// acepts numeric ids 
+router.get('/param/:id([0-9]+)/piso/:piso/puerta/:puerta(A|B|C)', (req, res, next) => {
+  console.log('req.params', req.params);
+  res.send('ok');
+});
+
+router.get('/enquerystring', [
+  query('age')
+    .isNumeric().withMessage('debería ser un número')
+    .custom((value) => {
+      if (value < 18) {
+        throw new Error('debe ser mayor de edad');
+      }
+      return true;
+    })
+  
+], (req, res, next) => {
+  validationResult(req).throw();
+  console.log('req.query', req.query);
+  const pelo = req.query.pelo;
+  const sexo = req.query.sexo;
+  res.send('ok');
+});
+
+router.post('/enelbody', (req, res, next) => {
+  console.log('req.body', req.body);
+  res.send('ok');
 });
 
 module.exports = router;
